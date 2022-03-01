b5e5530022be24059d5995af9f1d28586ba54488
hung4102 <97976728+hung4102@users.noreply.github.com>
2022-03-01 14:04:15 +0700

    Bai_thuc_hanh_2202
    Bai_thuc_hanh_2202

---
+++ Bai_Thuc_Hanh_2202/Bai_Thuc_Hanh_2202-main/Bai_Thuc_Hanh_2202/App11/src/App11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class App11 {
+    public static void main(String[] args) {
+        int i,j,k;
+        for(i=0;i<=20;i++)
+            for(j=0;j<=10;j++)
+                for(k=0;k<=4;k++)
+                    if(i*10000+j*20000+k*50000==200000)
+                        out.println(+i+" tờ 10000 "+j+" tờ 20000 "+ k+" tờ 50000");
+
+
+
+    }
+}
